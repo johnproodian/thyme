8ac55736f0776617f4976bd46447d0901b383c42
Jotas <jproodian721@gmail.com>
2022-09-22 10:24:18 -0500

    uncomment out build code for production

--- server/server.js
+++ server/server.js
@@ -43,9 +43,9 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../thyme-app/build')));
 } 
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../thyme-app/build/index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../thyme-app/build/index.html'));
+});
 
 db.once('open', () => {
     app.listen(PORT, () => {
