e1177bb03867dce14ba10e23add9ab6aaad60051
Jotas <jproodian721@gmail.com>
2022-10-17 14:07:49 -0500

    add Product and GroceryData models

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,5 @@
 const User  = require('./User');
+const Product = require('./Product');
+const GroceryData = require('./GroceryData');
 
-module.exports = { User };
+module.exports = { User, Product, GroceryData };
