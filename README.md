f827027b41538902da5d18eda343cffe9a707106
Jotas <jproodian721@gmail.com>
2022-10-24 20:58:36 -0500

    updated readme with some guidelines for authentication

--- README.md
+++ README.md
@@ -1,10 +1,50 @@
-# weneedabetternamethanactgroup40
+# Thyme
 
 ## Starting the App
 * Both servers simultaneously: run ```npm run develop``` in the root directory.
 * Just the client: run ```npm start``` in the thyme-app/ directory.
 * Just the server: run ```npm run watch``` in the server/ directory. To start it without nodemon, run ```npm start```.
 
+
+
+### Authentication (in addition to other authentication notes from earlier commit)
+
+On the front end, whenever authentication is needed--to conditionally render a page only for logged in users, or literally to log a user in or out--do the following:
+
+* Import from the auth module from the auth folder (something like):
+
+```
+import Auth from '../../../utils/auth' // adjust the path as necessary, obviously
+```
+
+* The mutation that logs a user in or signs them up will return a data object that includes a generated token. Get that token from the object and pass it through the login or method from Auth:
+
+```
+// probably something like:
+Auth.login(data.login.token);
+```
+
+* Similarly when a user needs to be logged out:
+
+```
+Auth.logout();
+```
+
+* If/when a page needs to be conditionally rendered (based on whether a user is logged in or not), you can do something like this to render the div only if the user is logged in:
+
+```
+const loggedIn = Auth.loggedIn();
+
+return (
+    <main>
+        {loggedIn && (
+            <div>You are logged in!</div>
+        )}
+
+    </main>
+)
+```
+    
 
 ## Overview of Authentication with JWTs
 Iyan and I (John) have been talking about using JSON web tokens for authentication. 
--- thyme-app/src/utils/auth.js
+++ thyme-app/src/utils/auth.js
@@ -1,9 +1,47 @@
 import decode from 'jwt-decode';
 
+// I think we only need to use login(), logout(), and loggedIn() explicitly in front-end code--unless special circumstances call for one of the others
 class AuthService {
 
-    // retrieve token from localStorage
-    getToken() {
+    // set token to localStorage and reload the page to homepage when a user logs in
+    login(idToken) {
+        localStorage.setItem('id_token', idToken);
+
+        window.location.assign('/');
+    }
+
+    // remove the token from localStorage, effectively logging the user out
+    logout() {
+        localStorage.clearItem('id_token');
+
+        window.location.assign('/');
+    }
+
+    // check to see if user is logged in by retrieving the token from localStorage--if there is a non-expired token, return true (that user is logged in)
+    loggedIn() {
+        const token = this.getToken();
+
+        // Double negation (I believe) to ensure that a boolean is always returned
+        return !!token && !this.isTokenExpired(token);
+    }
+
+     // retrieve token from localStorage
+     getToken() {
         return localStorage.getItem('id_token');
+    }
+
+    // check if token is expired; return true if expired
+    isTokenExpired() {
+        try {
+            // gets the object that was made into a token--including email, password, storeID, etc.
+            const decoded = decode(token);
+            if (decoded.exp < Date.now() / 1000) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (err) {
+            return false;
+        }
     }
 }
