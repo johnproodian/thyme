9c5beb8bb29effad9388cb89b8db2527d18596d4
StevenMain <merge.conflictstyle>
2022-11-02 10:04:18 -0500

    refactored and added error catching

--- python/main.py
+++ python/main.py
@@ -1,18 +1,26 @@
 # Main python file to run script that pulls item data every day and adds to mongo db
 
-import apiTools
-import time
+import apiTools as api
 
 # Check if token in valid and if not refresh token
 # Create a loop that runs once a day over multiple items
-# Might need t change pullitem to accept a token
+# Might need to change pullitem to accept a token
 # Run pullItem
 
-x = 0
-while x < 5:
-    time.sleep(5)
-    x = x + 1
-    print(x)
+test_items =['bread', 'water', 'milk']
+list_of_item_types =['beef', 'chicken','bread','fish','candy', 'milk',
+                    'juice', 'water', 'fruit', 'vegetable', 'soup', 'bakeware',
+                     'chips', 'soda', 'pork', 'crackers', 'diapers', 'cake']
+
+# Run a large batch of preslected items.  Also check to make sure that it is not grabbing a new token
+# unless a new token is needed.
+for item in test_items:
+    try:
+        token = api.pullItem(item, '03400738', '1', token)
+        print("I used the old token!")
+    except:
+        token = api.pullItem(item, '03400738', '1', "token")
+        print("I had to get a new token")
 
 
-print("hello")
+
