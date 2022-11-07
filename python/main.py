a66826785419f71e550a981385559871f9a47a02
StevenMain <merge.conflictstyle>
2022-11-07 13:05:13 -0600

    updated main.py

--- python/main.py
+++ python/main.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python
 # Main python file to run script that pulls item data every day and adds to mongo db
 
 import apiTools as api
+import subprocess
 
 # Check if token in valid and if not refresh token
 # Create a loop that runs once a day over multiple items
@@ -12,15 +14,23 @@ list_of_item_types =['beef', 'chicken','bread','fish','candy', 'milk',
                     'juice', 'water', 'fruit', 'vegetable', 'soup', 'bakeware',
                      'chips', 'soda', 'pork', 'crackers', 'diapers', 'cake']
 
+
 # Run a large batch of preslected items.  Also check to make sure that it is not grabbing a new token
 # unless a new token is needed.
-for item in test_items:
-    try:
-        token = api.pullItem(item, '03400738', '1', token)
-        print("I used the old token!")
-    except:
-        token = api.pullItem(item, '03400738', '1', "token")
-        print("I had to get a new token")
+def get_data(items, store_location, limit):
+    count = 0
+    for item in items:
+        try:
+            token = api.pullItem(item, store_location, limit, token)
+            print("I used the old token!")
+        except:
+            token = api.pullItem(item, store_location, limit, "token")
+            print("I had to get a new token")
+        
+        count +=1
 
+    print("There are " + str(count * limit) + " results")
+
+get_data(list_of_item_types, '03400738', "10")
 
 
